b8bd7fc270453124f5c441a3c92e4df23d6cd6b9
Mustafa Jassem <matehtr@gmail.com>
2026-04-26 22:35:13 +0300

    made the condition first param  in WhereIf instead of the second param in IQueryableExtensions

--- AspNetTemplate/Core/Infra/Extensions/IQueryableExtensions.cs
+++ AspNetTemplate/Core/Infra/Extensions/IQueryableExtensions.cs
@@ -28,8 +28,8 @@ public static class IQueryableExtensions
     }
 
     public static IQueryable<T> WhereIf<T>(this IQueryable<T> query,
-        Expression<Func<T, bool>> predicate,
-        bool condition)
+        bool condition,
+        Expression<Func<T, bool>> predicate)
     {
         return condition ? query.Where(predicate) : query;
     }
